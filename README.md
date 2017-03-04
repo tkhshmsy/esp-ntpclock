cf9e7caeac479a9d6881a23a1c805ac1e7f0ecfa
tkhshmsy <tkhshmsy@gmail.com>
2017-03-04 22:19:30 +0900

    using i2c display
        - 0.96inch OLED 128x64 Display with SSD1306

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ NTP sync Clock using ESP8266
 - H/W
     - SwitchScience ESPr-Developer
         - ESP8266 / ESP-WROOM-02
+    - OLED Display
+        - SSD1306 / 128x64
 - S/W
     - platformio
+        - library 
+            - ESP8266_SSD1306 (ID:562)
 
 ## Author
 - tkhshmsy@gmail.com
--- src/main.ino
+++ src/main.ino
@@ -2,10 +2,18 @@
 #include <ESP8266WiFi.h>
 #include <time.h>
 
+#include <Wire.h>
+#include <SSD1306.h>
+
 #include "wifi_secrets.h"
 #define JST (3600 * 9)
 
 const char *weekDays[7] = { "Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat" };
+char ipaddrBuffer[15 + 1];
+char dateBuffer[15 + 1];
+char clockBuffer[8 + 1];
+
+SSD1306 display(0x3c, 4, 5); //addr, SDA, SCK
 
 void setup()
 {
@@ -27,8 +35,20 @@ void setup()
     Serial.printf("Connected, IPADDR = ");
     Serial.println(WiFi.localIP());
 
+    byte oct0 = WiFi.localIP()[0];
+    byte oct1 = WiFi.localIP()[1];
+    byte oct2 = WiFi.localIP()[2];
+    byte oct3 = WiFi.localIP()[3];
+    sprintf(ipaddrBuffer, "IPADDR:%d.%d.%d.%d", oct0, oct1, oct2, oct3);
+
     //sync NTP
     configTime(JST, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
+
+    //configure SSD1306 i2c display
+    display.init();
+    display.flipScreenVertically();
+    display.setFont(ArialMT_Plain_10);
+    display.displayOn();
 }
 
 void loop()
@@ -37,9 +57,22 @@ void loop()
     struct tm *tm;
     t = time(NULL);
     tm = localtime(&t);
-    Serial.printf("%04d-%02d-%02d(%s) %02d:%02d:%02d\n",
+    sprintf(dateBuffer, "%04d-%02d-%02d(%s)",
                     tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday,
-                    weekDays[tm->tm_wday],
+                    weekDays[tm->tm_wday]);
+    sprintf(clockBuffer, "%02d:%02d:%02d",
                     tm->tm_hour, tm->tm_min, tm->tm_sec);
-    delay(500);
+
+    Serial.printf("%s %s\n", dateBuffer, clockBuffer);
+
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(64, 0, String(ipaddrBuffer));
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 16, String(dateBuffer));
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(64, 32 + 8, String(clockBuffer));
+    display.display();
+    delay(250);
 }
